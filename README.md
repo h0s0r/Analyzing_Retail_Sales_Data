35abd6b55609cd4bad63cabff2441de3322e4ccd
h0s0r <hardidar.ldh@gmail.com>
2025-07-25 19:16:55 +0530

    Completed - Three types of analysis on dataset done successfully with proper readme and code documented with comments

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
-# Analyzing_Retail_Sales_Data
+___
+___
+## __Exploratory Data Analysis (EDA)__ - 
+Will be analyzing a sales dataset from kaggle - {Analyzing_Retail_Sales_Data}
+___
+### Summary - 
+Imported a Sales data set from Kaggle and performed data analysis on it. Created multiple goal based manipulated datasets and plotted respective charts and plots based on manipulated datasets to analyse the market in multiple ways __[check the Info below for more details].__
+___
+### Modules/Libraries Used
 
-## Exploratory Data Analysis (EDA) with Pandas & Matplotlib
+- Pandas
+- Matplotlib
+- Seaborn
+___
+### Types of Analysis Performed On Dataset
 
-    Objective: Analyze a real-world sales dataset to uncover business insights, such as best-selling products and monthly sales trends.
-    Skills: Loading data from CSV, cleaning messy data, data aggregation (groupby), and creating meaningful visualizations (bar charts, line plots).
+- __Categorical Sales__ 
+  - category_sales - Result Dataset Name
+  - Categorical Sales - Bar Chart Title
+- __Monthly Sales__
+  - monthly_sales - Result Dataset Name
+  - Monthly Sales - Line Chart Title
+- __Sub Categorical Sales__
+  - sub_categorical_sales - Result Dataset Name
+  - Sub-Categorical Sales Data - Bar Chart Title
+___
+___
+By h0s0r - THE END
+___
+___
--- analysis.py
+++ analysis.py
@@ -11,15 +11,21 @@ file_path = 'train.csv'
 
 try:
     df = pd.read_csv(file_path) # Fetching the properly formatted dataset from csv file and storing it in a variable df
-    print('Data loaded successfully. DataFrame/DataSet Info Details : ', df.info()) # Fetching details related to the dataset using info()
+    print('Data loaded successfully. DataFrame/DataSet Info Details : ')
+    df.info() # Fetching details related to the dataset using info()
     print("\nFirst 5 rows of the dataset:")
     print(df.head()) # getting first five rows of data set as sample using head()
+
     print('/nMissing Values : ', df.isnull().sum()) # checking for null values using isnull() While sum() helps getting the number of it
     df.dropna(subset = ['Postal Code'], inplace=True) # removing null value rows from dataset using dropna() While subset asks for only missing values in Postal Code and inplace=True means if null is there in the place
-    print('/nMissing Values Handled: ',df.info()) #rechecking dataframe after handling missing values
-    category_sales = df.groupby('Category')['Sales'].sum().reset_index #Fetching categorical sales by selecting to group by category taking sum of sales in each category then reseting the index to return dataset to orignal state
-    category_sales = df.sort_values(by=['Sales'], ascending=False) # sorting the categorical sales data by descending order
+    print('/nMissing Values Handled: ')
+    df.info() #rechecking dataframe after handling missing values
+
+    category_sales = df.groupby('Category')['Sales'].sum().reset_index() #Fetching categorical sales by selecting to group by category taking sum of sales in each category then reseting the index to return dataset to orignal state
+    print(f'Category Sales Without Sort: \n{category_sales}')
+    category_sales = category_sales.sort_values(by=['Sales'], ascending=False) # sorting the categorical sales data by descending order
     print(f'Categorical Sales DataSet : {category_sales}') # returning properly formatter categorical sales data
+
     print('Plotting Data : ')
     plt.figure(figsize = (10,6)) #plotting a graph of size x=10,y=6
     sns.barplot(
@@ -33,6 +39,7 @@ try:
     plt.xlabel('Category')
     plt.ylabel('Sales')
     plt.show()
+
     df['Order Date'] = pd.to_datetime(df['Order Date'], format = '%d/%m/%Y') # Conversion of Order Date to proper DateTime format.
     df['Order Month'] = df['Order Date'].dt.to_period('M') # Adding a column that will contain then month of sale
     print('Converted Order Date to datetime format and added Order Month Column')
@@ -40,6 +47,7 @@ try:
     monthly_sales['Order Month'] = monthly_sales['Order Month'].astype(str) # Converting Order Month back to str to create a chart
     print(f'Monthly Sales DataSet : {monthly_sales}')
     print('Plotting monthly sales trend data : ')
+
     plt.figure(figsize = (15,7)) # setting size of chart of 15*7
     sns.lineplot(x='Order Month',y='Sales', data=monthly_sales, marker='o')
     plt.title('Monthly Sales')
@@ -47,6 +55,21 @@ try:
     plt.ylabel('Sales')
     plt.xticks(rotation=45)
     plt.tight_layout()
+    plt.show()
+
+    sub_categorical_sales = df.groupby('Sub-Category')['Sales'].sum().reset_index()
+    sub_categorical_sales = sub_categorical_sales.sort_values(by=['Sales'], ascending=False)
+    print(f'Sub-Categorical Sales DataSet : {sub_categorical_sales}'
+          f'\nSub-Categorical Sales DataSet Info : ')
+    sub_categorical_sales.info()
+    print('Plotting sub-categorical sales data : ')
+    plt.figure(figsize = (17,10))
+    sns.barplot(x='Sub-Category',y='Sales', data=sub_categorical_sales)
+    plt.title('Sub-Categorical Sales Data')
+    plt.xlabel('Sub-Category')
+    plt.ylabel('Sales')
+    plt.tight_layout()
+    plt.xticks(rotation=45)
     plt.show()
 
 except FileNotFoundError:
